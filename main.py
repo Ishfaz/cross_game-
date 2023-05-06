835be84abb480db88f057b9c0f1f55b84aa81d85
Ishfaq Ahmad Bhat <96306723+Ishfaz@users.noreply.github.com>
2023-05-06 13:01:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player)<20:
             game_is_on=False
+            scoreboard.game_over()
 #succesful completetion
     if player.is_at_finish_line():
         player.go_to_start()
